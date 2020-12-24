5c88702c51c641a5b2ebdae74f35aa709a15fc23
WarioNeila86 <mariogar1001@gmail.com>
2020-12-24 14:16:01 +0100

    Save state of the Squares in the Board

--- src/index.js
+++ src/index.js
@@ -3,29 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    // All React component classes that have a constructor should start with a super(props) call.
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
-
   render() {
     return (
       <button
         className="square"
-        onClick={() => this.setState({ value: 'X' })}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.splice();
+    squares[i] = 'X';
+    this.setState({squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
